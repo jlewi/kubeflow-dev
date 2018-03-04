587f75a314fb0f03c9c3dd78aef5c18bfc53e542
Jeremy Lewi <jeremy@lewi.us>
2018-03-03 21:27:24 -0800

    update to build GPU image.

--- run_tf_serving_release.sh
+++ run_tf_serving_release.sh
@@ -4,15 +4,25 @@ PULL_BASE_SHA="master"
 DATE=`date +%Y%m%d`
 VERSION_TAG="v${DATE}-${PULL_BASE_SHA}"
 JOB_NAME="tf-serving-release"
-JOB_TYPE=postsubmit
 BUILD_NUMBER=$(uuidgen)
 BUILD_NUMBER=${BUILD_NUMBER:0:4}
 REPO_OWNER=kubeflow
 REPO_NAME=kubeflow
 
 ENV=releasing
-ks param set --env=${ENV} workflows namespace kubeflow-releasing
+
+JOB_TYPE=presubmit
+PULL_NUMBER=339
+PULL_PULL_SHA=72bc7b1
+
+PROW_VAR="JOB_NAME=${JOB_NAME},JOB_TYPE=${JOB_TYPE},REPO_NAME=${REPO_NAME},REPO_OWNER=${REPO_OWNER},BUILD_NUMBER=${BUILD_NUMBER}" 
+
+if [ "${JOB_TYPE}" == "presubmit" ]; then
+	PROW_VAR="${PROW_VAR},PULL_NUMBER=${PULL_NUMBER},PULL_PULL_SHA=${PULL_PULL_SHA}"
+	VERSION_TAG="v${DATE}-pr${PULL_NUMBER}-${PULL_PULL_SHA}-${BUILD_NUMBER}"	
+fi
+
 ks param set --env=${ENV} workflows name "${USER}-${JOB_NAME}-${VERSION_TAG}-${BUILD_NUMBER}"
-ks param set --env=${ENV} workflows prow_env "JOB_NAME=${JOB_NAME},JOB_TYPE=${JOB_TYPE},PULL_BASE_SHA=${PULL_BASE_SHA},REPO_NAME=${REPO_NAME},REPO_OWNER=${REPO_OWNER},BUILD_NUMBER=${BUILD_NUMBER}"
-ks param set --env=${ENV} workflows serving_image "gcr.io/kubeflow-images-staging/tf-model-server:${VERSION_TAG}"
+ks param set --env=${ENV} workflows prow_env "${PROW_VAR}"
+ks param set --env=${ENV} workflows versionTag "${VERSION_TAG}"
 ks apply ${ENV} -c workflows

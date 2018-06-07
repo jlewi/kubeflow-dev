606b7ca15b29b114fc5f81e466b3f63a81e1e759
Jeremy Lewi <jeremy@lewi.us>
2018-06-07 14:29:07 -0700

    Latest configs.

--- kubeflow-deployments/env-kubeflow-jlewi.sh
+++ kubeflow-deployments/env-kubeflow-jlewi.sh
@@ -9,15 +9,17 @@
 # Set PROJECT to the project you want to use with Kubeflow.
 export PROJECT=cloud-ml-dev
 
+export DEPLOYMENT_NAME=jlewi-kubeflow
+
 # The name of the ip address as defined in cluster.jinja
 # We will reserve a GCP IP address to use for ingress to the cluster.
 export IP_NAME=static-ip
 
 # Set the namespace to the name of the namespace to deploy Kubeflow in.
 export NAMESPACE=kubeflow
-
+export ZONE=us-east1-d
 # Set config file to the YAML file defining your deployment manager configs.
-export CONFIG_FILE=cluster-kubeflow.yaml
+export CONFIG_FILE=~/git_kubeflow-dev/kubeflow-deployments/cluster-cloud-ml-dev-jlewi.yaml
 export PROJECT_CONFIG_FILE=project-${PROJECT}.yaml
 
 # ksonnet environment

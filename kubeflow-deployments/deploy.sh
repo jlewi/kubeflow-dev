fc21cb7ed2a69505ce9dcac0f8ecbe3b1fc4fce7
Jeremy Lewi <jeremy@lewi.us>
2018-06-07 23:15:01 -0700

    Create scripts to automate deploying my endpoint.

--- kubeflow-deployments/deploy.sh
+++ kubeflow-deployments/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # A script to facilitate redeploying Kubeflow using the latest templates.
+#
 
 set -ex
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -10,7 +11,21 @@ cp -f ~/git_kubeflow/docs/gke/configs/cluster-kubeflow.yaml  ./
 cp -f ~/git_kubeflow/docs/gke/configs/cluster.jinja  ./
 
 # Source environment variables containing client id and secret
+.  env-kubeflow-jlewi.sh
 . ~/secrets/jlewi-kubeflow-endpoints-oauth.sh
 
+python modify_yaml.py
 
-jq -r ".metadata.annotations.iaplock=\"$(hostname -s) ${NOW}\"" service.json > service_lock.json
+gcloud deployment-manager --project=${PROJECT} deployments create ${DEPLOYMENT_NAME} --config=${CONFIG_FILE}
+   
+gcloud --project=${PROJECT} container clusters get-credentials --zone=${ZONE} ${DEPLOYMENT_NAME}
+
+# create_k8s_secrets
+# Delete any of the previous service account keys
+rm *iam.gserviceaccount.com.json
+~/git_kubeflow/docs/gke/create_k8s_secrets.sh 
+# Create oauth secrets
+NAMESPACE=kubeflow
+kubectl create secret -n ${NAMESPACE} generic kubeflow-oauth --from-literal=CLIENT_ID=${CLIENT_ID} --from-literal=CLIENT_SECRET=${CLIENT_SECRET}
+
+../create_context.sh jlewi-kubeflow ${NAMESPACE}

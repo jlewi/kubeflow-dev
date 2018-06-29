2801a81890afb6bb61d2db26c83b00c3e7018f18
Jeremy Lewi <jeremy@lewi.us>
2018-06-29 08:46:13 -0700

    Create a script to update the ksonnet app * Update to rc 0.2.1

---
+++ kubeflow-deployments/upgrade_deployment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Upgrade a Kubeflow app to a given version.
+#
+
+set -ex
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd ${DIR}/ks-app
+
+# Run the upgrade script.
+# TODO(jlewi): We might want to specify the registry version.
+~/git_kubeflow/scripts/upgrade_ks_app.py 
+
+# Regenerate the core component
+ks component rm kubeflow-core
+ks generate kubeflow-core kubeflow-core

84ef995d56ce8dadbb76c192866766635f48c115
Jeremy Lewi <jeremy@lewi.us>
2018-07-06 15:25:40 -0700

    Delete script to retag kaggle_image because it doesn't work.

--- retag_kaggle_image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Command doesn't appear to work; looks like it overloads some gcloud buffer.
-gcloud container images add-tag --quiet \
-	gcr.io/kubeflow-dev/kubeflow-kaggle-notebook@sha256:6a7e3423c3a8db391bc6d3e48356bbc43ed4126a400f3c80d3de005e7df80f99 \
-	gcr.io/kubeflow-images-public/kaggle-notebook:v20180629

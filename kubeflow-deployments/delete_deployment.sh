3f63fb296e2c0516f807ef1baa6e9e724defc5bf
Jeremy Lewi <jeremy@lewi.us>
2018-07-11 11:43:26 -0700

    Update the delete script.
    * Use abandon policy if delete fails
    * Manually delete resources to clean it up.

--- kubeflow-deployments/delete_deployment.sh
+++ kubeflow-deployments/delete_deployment.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
-set -ex 
+set -x 
 .  env-kubeflow-jlewi.sh
 
-gcloud deployment-manager --project=${PROJECT} deployments delete ${DEPLOYMENT}
 
-# TODO(jlewi): Delete service accounts and other resources to cleanup a failed
-# deployment manager delete.
+gcloud deployment-manager --project=${PROJECT} deployments delete \
+	${DEPLOYMENT_NAME} \
+	--quiet
+
+RESULT=$?
+
+set -e
+
+if [ ${RESULT} -ne 0 ]; then
+	echo deleting the deployment did not work retry with abandon
+	gcloud deployment-manager --project=${PROJECT} deployments delete \
+	${DEPLOYMENT_NAME} \
+	--quiet \
+	--delete-policy=abandon
+
+fi
+
+gcloud --project=${PROJECT} container clusters delete --zone=${ZONE} \
+	${DEPLOYMENT_NAME} --quiet
+
+# Delete service accounts
+declare -a accounts=("vm" "admin" "user")
+
+# now loop through the above array
+for suffix in "${accounts[@]}"
+do   
+   gcloud --project=${PROJECT} iam service-accounts delete \
+	${DEPLOYMENT_NAME}-${suffix}@${PROJECT}.iam.gserviceaccount.com \
+	--quiet	
+done

4945598d0e78538b9e5cbb7e154b971b88075dc6
Jeremy Lewi <jeremy@lewi.us>
2018-07-11 11:13:56 -0700

    latest.

--- kubeflow-deployments/upgrade_deployment.sh
+++ kubeflow-deployments/upgrade_deployment.sh
@@ -2,6 +2,7 @@
 #
 # Upgrade a Kubeflow app to a given version.
 #
+# TODO(jlewi)" Looks at the script for dev.kubeflow.org
 
 set -ex
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -11,6 +12,7 @@ cd ${DIR}/ks-app
 # Run the upgrade script.
 # TODO(jlewi): We might want to specify the registry version.
 ~/git_kubeflow/scripts/upgrade_ks_app.py 
+
 
 # Regenerate the core component
 ks component rm kubeflow-core

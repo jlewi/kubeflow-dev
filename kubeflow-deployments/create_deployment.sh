643b3f80fb33fd58695bfdce699d518539e5ad79
Jeremy Lewi <jeremy@lewi.us>
2018-07-30 15:39:59 -0700

    Update create_deployment.sh to handle TAG and version consistently.

--- kubeflow-deployments/create_deployment.sh
+++ kubeflow-deployments/create_deployment.sh
@@ -6,13 +6,13 @@ set -ex
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 PROJECT=$1
-# Do not include "v" in version"
-VERSION=$2
+# Tag to checkout
+KUBEFLOW_TAG=$2
 DEPLOYMENT_NAME=$3
 DEPLOY=$4
 
-if [ -z ${VERSION} ]; then
-	echo "usage create_deployment.sh <version>"
+if [ -z ${KUBEFLOW_TAG} ]; then
+	echo "usage create_deployment.sh <project> <kubeflow_tag> <DEPLOYMENT_NAME> <DEPLOY>"
 	exit 1
 fi	
 
@@ -20,6 +20,7 @@ if [ -z ${DEPLOY} ]; then
 	DEPLOY=true
 fi
 
+KUBEFLOW_VERSION=${KUBEFLOW_TAG//v/}
 VERSION_NAME=${KUBEFLOW_VERSION//./-}
 
 cd ${DIR}
@@ -53,6 +54,7 @@ SETUP_PROJECT=${SETUP_PROJECT:false}
 # Output the environment variables
 cat > ${DEPLOY_DIR}/env.sh << EOF
 export PROJECT=${PROJECT}
+export KUBEFLOW_TAG=${KUBEFLOW_TAG}
 export KUBEFLOW_VERSION=${VERSION}
 export KUBEFLOW_DM_DIR=${DEPLOY_DIR}/dm-configs
 export KUBEFLOW_KS_DIR=${DEPLOY_DIR}/ks-app
@@ -65,7 +67,7 @@ if [ "${VERSION}" == "local" ]; then
 	cd ${DEPLOY_DIR}/scripts/gke
 	./deploy.sh
 else
-	curl -o ${DEPLOY_DIR}/deploy.sh https://raw.githubusercontent.com/kubeflow/kubeflow/v${KUBEFLOW_VERSION}/scripts/gke/deploy.sh 
+	curl -o ${DEPLOY_DIR}/deploy.sh https://raw.githubusercontent.com/kubeflow/kubeflow/${KUBEFLOW_TAG}/scripts/gke/deploy.sh 
     cd ${DEPLOY_DIR}
     chmod a+x deploy.sh
     ./deploy.sh

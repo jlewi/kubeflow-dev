9c893586ff9e4d3f3c2ab2973bdbe1cb55040cbf
Jeremy Lewi <jeremy@lewi.us>
2019-06-18 20:43:28 -0700

    Create a simple goprogram to test IAM permissions.

---
+++ gocode/go.mod
@@ -0,0 +1,8 @@
+module github.com/jlewi/kubeflow-dev/bootstrap
+
+go 1.12
+
+require (
+	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
+	google.golang.org/api v0.6.0
+)
---
+++ gocode/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	crm "google.golang.org/api/cloudresourcemanager/v1"
+	"log"
+)
+
+// Pformat returns a pretty format output of any value.
+func Pformat(value interface{}) (string, error) {
+	if s, ok := value.(string); ok {
+		return s, nil
+	}
+	valueJson, err := json.MarshalIndent(value, "", "  ")
+	if err != nil {
+		return "", err
+	}
+	return string(valueJson), nil
+}
+
+func main() {
+
+	ctx := context.Background()
+	//client, err := google.DefaultClient(ctx, crm.CloudPlatformScope)
+	//
+	//s, err := crm.NewService(ctx, client)
+	//
+	//crm.NewFoldersService(client)
+	//
+	//crm.Ne
+	s, err := crm.NewService(ctx)
+
+	if err != nil {
+		log.Fatalf("Failed with error; %+v", err)
+	}
+
+	p := crm.NewProjectsService(s)
+
+	req := &crm.TestIamPermissionsRequest{
+		Permissions: []string{"resourcemanager.projects.setIamPolicy",},
+	}
+
+	res, err  := p.TestIamPermissions("kubeflow-dev", req).Do()
+
+	if err != nil {
+		log.Fatalf("Error: %+v", err)
+	}
+
+	pRes, _ := Pformat(res)
+	fmt.Printf("Result: %+v", pRes)
+}
+
+

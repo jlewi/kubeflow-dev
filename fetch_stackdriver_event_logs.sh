daf17bf6a3ea1f59e81dd3d28fdfaebbb399344b
Jeremy Lewi <jlewi@google.com>
2019-10-22 08:31:18 -0700

    Update scripts to fetch logs and run kfctl.

--- fetch_stackdriver_event_logs.sh
+++ fetch_stackdriver_event_logs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Fetch pod event logs for the test cluster.
 set -ex
-POD=$1
+project=$1
+POD=$2
 
-FILTER="resource.labels.cluster_name=\"kubeflow-testing\""
+FILTER="resource.labels.cluster_name=\"${PROJECT}\""
 FILTER="${FILTER} resource.type=\"k8s_pod\" resource.labels.pod_name=\"${POD}\" "
 
 
@@ -11,11 +12,12 @@ FILTER="${FILTER} resource.type=\"k8s_pod\" resource.labels.pod_name=\"${POD}\" 
 # filtering just by pod_id appears to work though
 #"resource.type=\"container\" and resource.labels.cluster_name=\"${CLUSTER}\" and resource.labels.pod_id=\"${POD}\" and resource.labels.container_name=\"main\""
 #--format="table(timestamp, resource.labels.pod_name, jsonPayload.reason, jsonPayload.message)"
-gcloud --project=kubeflow-ci logging read  \
+LOGFILE=/home/jlewi/tmp/${POD}.event.log.txt
+gcloud --project=${PROJECT} logging read  \
 	--format="table(timestamp, resource.labels.pod_name, jsonPayload.reason, jsonPayload.message)" \
 	--freshness=24h \
 	--order asc \
-    "${FILTER}" > /tmp/$1.event.log.txt
+    "${FILTER}" > ${LOGFILE}
 
-cat /tmp/$1.event.log.txt
-echo Logs written to /tmp/$1.event.log.txt
+cat ${LOGFILE}
+echo Logs written to ${LOGFILE}
--- run_kfctl.sh
+++ run_kfctl.sh
@@ -4,23 +4,34 @@
 # Intended for quick iteration during development
 set -ex
 
-BOOTSTRAPDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null && pwd )"
+BOOTSTRAPDIR=/home/jlewi/git_kubeflow-kubeflow/bootstrap
 
 APPNAME=kftest-$(date +%m%d-%H%M%S)
-APPDIR=/tmp/${APPNAME}
+APPDIR=/home/jlewi/git_jlewi-kubeflow-dev/kubeflow-deployments/${APPNAME}
+
+# Source iap secrets
+. ~/secrets/jlewi-dev.oauth.sh 
 
 cd ${BOOTSTRAPDIR}
 make build-kfctl
 
 KFCTL=${BOOTSTRAPDIR}/bin/kfctl
-${KFCTL} init ${APPDIR} \
-  --platform=gcp \
-  --skip-init-gcp-project --disable_usage_report \
-  --project=jlewi-dev --use_istio
-  
+
+mkdir -p ${APPDIR}
+cp /home/jlewi/git_kubeflow-manifests/kfdef/kfctl_gcp_iap.yaml ${APPDIR}/
+
+CFGFILE=${APPDIR}/kfctl_gcp_iap.yaml
+
+# Zero out the name 
+PROJECT=jlewi-dev
+ZONE=us-east1-d
+
+yq w -i ${CFGFILE} metadata.name "" 
+yq w -i ${CFGFILE} spec.project ${PROJECT}   # Work around for 
+yq w -i ${CFGFILE} spec.zone ${ZONE}   # Work around for https://github.com/kubeflow/kubeflow/issues/3703
+ 
+# Use local manifests
+yq w -i ${CFGFILE} spec.repos[0].uri /home/jlewi/git_kubeflow-manifests
 #--use_basic_auth
 
-
-cd ${APPDIR}
-${KFCTL} generate all -V
-${KFCTL} apply all -V
+${KFCTL} apply all -V  -f ${CFGFILE}

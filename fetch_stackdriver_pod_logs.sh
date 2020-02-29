3f5489b5422c2afa0cd4ee174d89b6432a380995
Jeremy Lewi <jlewi@google.com>
2020-02-28 16:39:49 -0800

    add order.

--- fetch_stackdriver_pod_logs.sh
+++ fetch_stackdriver_pod_logs.sh
@@ -4,11 +4,13 @@ set -ex
 PROJECT=$1
 POD=$2
 FRESHNESS="${FRESHNESS:-24h}"
+# Displaying in assending order appears to slow things down
+ORDER="${ORDER:-asc}"
 OUTFILE=~/tmp/${POD}.log.txt
 gcloud logging read --format="table(timestamp, resource.labels.container_name, textPayload)" \
 	--project=${PROJECT} \
 	--freshness=${FRESHNESS} \
-	--order asc  \
+	--order ${ORDER}  \
 	"resource.type=\"k8s_container\" resource.labels.pod_name=\"${POD}\"  " > ${OUTFILE}
 
 cat ${OUTFILE}
